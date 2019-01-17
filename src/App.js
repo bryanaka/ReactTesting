23b68bbb769e7ad4cfa299bfa481b1eb0b0daad6
Case Van Arkel <casevanarkel@gmail.com>
2019-01-16 16:53:41 -0800

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Testing extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Testing this out.</div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
 
-export default Testing;
+class Testing extends Component {
+  render() {
+    return (
+      <div>Testing this out.</div>
+    );
+  }
+}
+
+
+export default App;
