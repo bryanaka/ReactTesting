17cde142e78e29e54ece2b2cddcc1a0fabd8ec87
Case Van Arkel <casevanarkel@gmail.com>
2019-01-17 16:18:12 -0800

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -45,9 +45,9 @@ class Button extends Component {
     let button;
 
     if (isToggleOn) {
-      button = <button onClick={this.handleClick}>Turn button off</button>
+      button = <button onClick={this.handleClick}>Turn off</button>
     } else {
-      button = <button onClick={this.handleClick}>Turn button on</button>
+      button = <button onClick={this.handleClick}>Turn on</button>
     }
 
     return (
