5296b49e5f2a69a820ad65c8726a99092b483054
Case Van Arkel <casevanarkel@gmail.com>
2019-01-17 21:04:46 -0800

    debugging nav

--- src/App.js
+++ src/App.js
@@ -6,56 +6,61 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Button />
+        <Body />
       </div>
     );
   }
 }
 
-class Button extends Component {
+class Body extends Component {
+  render() {
+    return (
+      <Nav />
+    );
+  }
+}
 
+class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
+    this.state = {view: 'home'};
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.setState(state => ({ isToggleOn: !state.isToggleOn }));
-  }
+  handleClick(view) {
+    if (view == 'home') {
+      this.setState(state => ({ view: 'home' }));
+    }
+    else if (view == 'work') {
+      this.setState(state => ({ view: 'work' }));
+    }
+    else if (view == 'about') {
+      this.setState(state => ({ view: 'about' }));
+    }
+  } 
 
   render() {
-    const isToggleOn = this.state.isToggleOn;
-    let button;
-
-    if (isToggleOn) {
-      button = <button onClick={this.handleClick}>Turn off</button>
-    } else {
-      button = <button onClick={this.handleClick}>Turn on</button>
-    }
-
     return (
-      <div>
-        {button}
+      <div className="Nav-container">
+        <button className="Nav-button" onClick={this.handleClick('home')}>
+          Home
+        </button>
+        <button className="Nav-button" onClick={this.handleClick('work')}>
+          Work
+        </button>
+        <button className="Nav-button" onClick={this.handleClick('about')}>
+          About
+        </button>
+        <button className="Nav-button" onClick={this.handleClick('contact')}>
+          Contact
+        </button>
       </div>
+      <div>{this.state.view}</div>
     );
   }
 }
+
 
 export default App;
